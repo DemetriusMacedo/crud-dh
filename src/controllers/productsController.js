541c9a66e31bd3f1d3b07b3809f3274188df5ec0
Demetrius Macêdo <18658717+DemetriusMacedo@users.noreply.github.com>
2022-07-07 20:40:48 -0300

    exercicio crud segunda aula

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,42 +1,71 @@
 const fs = require('fs');
 const path = require('path');
+const { formatCurrencyBr } = require('../helpers/formatCurrencyBr');
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const productsFilePath = path.join(__dirname, '..', 'data', 'productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
 	// Root - Show all products
-	index: (req, res) => {
+	index: (request, response) => {
 		// Do the magic
 	},
+	detail: (request, response) => {
+		const { id } = request.params;
 
-	// Detail - Detail from one product
-	detail: (req, res) => {
-		// Do the magic
+		const productFound = products
+			.find((product) => product.id === +id);
+
+
+		const finalDiscount = (productFound.price * productFound.discount) / 100;
+		const finalPrice = productFound.price - finalDiscount;
+		const finalPriceFormated = formatCurrencyBr(finalPrice)
+		const price = formatCurrencyBr(productFound.price);
+
+		const productFinal = {
+			...productFound,
+			price,
+			finalPriceFormated
+		};
+
+		response.render('detail', { product: productFinal })
 	},
 
 	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
+	create: (request, response) => {
+		response.render('product-create-form')
 	},
 
 	// Create -  Method to store
-	store: (req, res) => {
+	store: (request, response) => {
 		// Do the magic
 	},
 
 	// Update - Form to edit
-	edit: (req, res) => {
+	edit: (request, response) => {
 		// Do the magic
 	},
 	// Update - Method to update
-	update: (req, res) => {
+	update: (request, response) => {
 		// Do the magic
 	},
 
 	// Delete - Delete one product from DB
-	destroy: (req, res) => {
-		// Do the magic
+	destroy: (request, response) => {
+		const { id } = request.params;
+		console.log(productsFilePath);
+		const newProducts = products
+			.filter((product) => product.id !== +id);
+
+		console.log(JSON.stringify(newProducts));
+		console.log(newProducts);
+
+		fs.writeFileSync(
+			productsFilePath,
+			JSON.stringify(newProducts)
+		);
+
+		response.redirect('/');
 	}
 };
 
