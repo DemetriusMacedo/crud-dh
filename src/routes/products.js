ca3312772c1e78664e09d73d411b88a906a2a790
tsunodajapa <gabrieltsunoda@gmail.com>
2022-07-07 21:39:09 -0300

    feat: upload file

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+
+const storage = require('../configs/multer');
+
 const router = express.Router();
+const upload = multer({ storage });
 
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.index);
 router.get('/create', productsController.create);
-router.post('/', productsController.store);
+router.post('/', upload.single('image'), productsController.store);
 router.get('/:id', productsController.detail);
 router.get('/:id/edit', productsController.edit);
 router.put('/:id', productsController.update);
